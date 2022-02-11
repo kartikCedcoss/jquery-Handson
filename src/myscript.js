609dbb37f5ba0c3091d50181e1b9f1e90043f028
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-11 17:25:43 +0530

    Use a jQuery method to slide up a <div> element.

--- src/myscript.js
+++ src/myscript.js
@@ -91,3 +91,7 @@ fadetoggle
 1000
 );
 });//Use a jQuery method to toggle between fading in and out a <div> element, when clicking on a button.
+
+$("div").
+slideUp
+();//Use a jQuery method to slide up a <div> element.
