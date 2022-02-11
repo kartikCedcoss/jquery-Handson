013e5e0c62b19a6038964d18074331eaaebe6c58
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-11 17:50:49 +0530

    Use a jQuery method to return the text content of a <div> element

--- src/myscript.js
+++ src/myscript.js
@@ -1,142 +1,85 @@
-$(document).ready(function(){
-    
-      $("[href]").hide();
-    
-  }); //Use the correct selector to hide all elements with an href attribute..
-  
-  $("tr:odd").hide(); //Use the correct selector to hide all odd table rows in a table.
+$(document).ready(function () {
+  $("[href]").hide();
+}); //Use the correct selector to hide all elements with an href attribute..
 
+$("tr:odd").hide(); //Use the correct selector to hide all odd table rows in a table.
 
-$("p").
-click
-(function(){
+$("p").click(function () {
   $(this).hide();
-});//Use the correct event to hide all <p> elements with a "click".
+}); //Use the correct event to hide all <p> elements with a "click".
 
-$("p").
-dblclick
-(function(){
+$("p").dblclick(function () {
   $(this).hide();
-});//Use the correct event to hide all <p> elements with a "double-click".
+}); //Use the correct event to hide all <p> elements with a "double-click".
 
-
-$("p").
-mouseenter
-(function(){
+$("p").mouseenter(function () {
   $(this).hide();
-});//When the mouse pointer enters a <p> element, it should be hidden. Use the correct event to do so.
+}); //When the mouse pointer enters a <p> element, it should be hidden. Use the correct event to do so.
 
-$("input").
-keypress
-(function(){
+$("input").keypress(function () {
   $(this).hide();
-});//If you press a keyboard key inside an <input> element, it should be hidden. Use the correct event to do so.
+}); //If you press a keyboard key inside an <input> element, it should be hidden. Use the correct event to do so.
 
-$("p").on(
-"click"
-, function(){
+$("p").on("click", function () {
   $(this).hide();
-});//Use the on() method to attach a click event handler to all <p> elements.
+}); //Use the on() method to attach a click event handler to all <p> elements.
 
-$("p").click(function(){
-    $(this).
-  hide
-  ();
-  });//Use a jQuery method to hide a <p> element when it is clicked on.
+$("p").click(function () {
+  $(this).hide();
+}); //Use a jQuery method to hide a <p> element when it is clicked on.
 
-  $("p").click(function(){
-    $(this).
-  hide
-  (
+$("p").click(function () {
+  $(this).hide("slow");
+}); //Use a jQuery method to hide a <p> element when it is clicked on.
+
+$("button").click(function () {
+  $("p").show();
+}); //Use a jQuery method to show a <p> element.
+
+$("button").click(function () {
+  $("p").toggle();
+}); //Use a jQuery method to toggle between hiding and showing a <p> element.
+
+$("div").fadeout(); //Use a jQuery method to fade out a <div> element.
+$("div").fadeOut("slow"); //Use a jQuery method to fade out a <div> element.
+
+$("div").fadeTo("slow", 0.2); //Use a jQuery method to fade a <div> element to an opacity of "0.2".
+
+$("button").click(function () {
+  $("div").fadetoggle(1000);
+}); //Use a jQuery method to toggle between fading in and out a <div> element, when clicking on a button.
+
+$("div").slideUp(); //Use a jQuery method to slide up a <div> element.
+
+$("div").slideUp("slow"); //Use a jQuery method to slide up a <div> element.
+
+$("div").slideDown(); //Use a jQuery method to slide down a <div> element.
+
+$("button").click(function () {
+  $("div").slideToggle();
+}); //Use a jQuery method to toggle between sliding up and down a <div> element, when clicking on a button.
+
+$("div").animate({
+  left: "250px",
+}); //Use the animate() method to move a <div> element 250 pixels to the right.
+
+$("div").animate({
+  height: "500px",
+}); //Use the animate() method to set the height of a <div> element to 500 pixels.
+
+$("div").animate({
+  opacity: "  0.4 ",
+  height: " 500px ",
+  width: "500 px",
+}); //Use the animate() method to set the following CSS properties for <div>:
+
+$("div").animate(
+  {
+    fontSize: " 100px ",
+  },
   "slow"
-  );
-  });//Use a jQuery method to hide a <p> element when it is clicked on.
+); //Use the animate() method to set the font-size of a <div> element to 100 pixels:
 
-  $("button").click(function(){
-    $("p").
-  show
-  ();
-  });//Use a jQuery method to show a <p> element.
+$("div").stop(); //Use a jQuery method to stop the animation effect of a <div> element.
 
-  $("button").click(function(){
-    $("p").
-  toggle
-  ();
-  });//Use a jQuery method to toggle between hiding and showing a <p> element.
-
-  $("div").
-fadeout
-();//Use a jQuery method to fade out a <div> element.
-$("div").
-fadeOut
-(
-"slow"
-);//Use a jQuery method to fade out a <div> element.
-
-$("div").
-fadeTo
-(
-"slow"
-, 
-0.2
-);//Use a jQuery method to fade a <div> element to an opacity of "0.2".
-
-
-$("button").
-click
-(function(){
-  $("div").
-fadetoggle
-(
-1000
-);
-});//Use a jQuery method to toggle between fading in and out a <div> element, when clicking on a button.
-
-$("div").
-slideUp
-();//Use a jQuery method to slide up a <div> element.
-
-$("div").
-slideUp
-(
-"slow"
-);//Use a jQuery method to slide up a <div> element.
-
-$("div").
-slideDown
-();//Use a jQuery method to slide down a <div> element.
-
-
-$("button").
-click
-(function(){
-  $("div").
-slideToggle
-();
-});//Use a jQuery method to toggle between sliding up and down a <div> element, when clicking on a button.
-
-$("div").animate({
-    left
-    : '250px'});//Use the animate() method to move a <div> element 250 pixels to the right.
-
-    $("div").animate({
-        height
-        : '500px'});//Use the animate() method to set the height of a <div> element to 500 pixels.
-
-
-$("div").animate({
-  
-    opacity : '  0.4 ',  height: ' 500px ', width: '500 px'});//Use the animate() method to set the following CSS properties for <div>:
-
-
-
-    $("div").animate({
-        fontSize
-        : ' 100px '}, "slow"); //Use the animate() method to set the font-size of a <div> element to 100 pixels:
-
-
-        $("div").
-        stop
-        ();//Use a jQuery method to stop the animation effect of a <div> element.
-        
-
+$("div").text(); //Use a jQuery method to return the text content of a <div> element
