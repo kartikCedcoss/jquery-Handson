deea9aa5bb42efaf9ff2f4b8e4f9515bad7a0cb3
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-11 17:12:47 +0530

    Use the correct event to hide all <p> elements with a double-click.

--- src/myscript.js
+++ src/myscript.js
@@ -12,3 +12,9 @@ click
 (function(){
   $(this).hide();
 });//Use the correct event to hide all <p> elements with a "click".
+
+$("p").
+dblclick
+(function(){
+  $(this).hide();
+});//Use the correct event to hide all <p> elements with a "double-click".
